56da40f9ca3083ed9053091ce6f65f058d4b98f4
Mancaster06 <rajabuomary061@gmail.com>
2024-02-27 00:06:59 -0800

    Create user_input.py

---
+++ user_input.py
@@ -0,0 +1,6 @@
+name = input("Enter your name:")
+age = input("Enter your age:")
+location = input("Enetr your location:")
+
+message = "Hello  "  + name + ", you are  " + age + " years old and you live in  " +location
+print(message)
